34579ba325afd5234742c5ea2e422c83c7a6ffba
SwastikaBarui <baruis040929@gmail.com>
2026-07-24 08:33:33 +0530

    Update 10_📋 Project Summary.py

--- pages/10_📋 Project Summary.py
+++ pages/10_📋 Project Summary.py
@@ -62,3 +62,8 @@ ARIMA modelling, and forecasting. The analysis also highlights practical challen
 
 st.divider()
 st.caption("💡 Skills Applied : Python | Numpy | Pandas | Matplotlib | Streamlit | Statsmodels.tsa ")
+
+
+st.markdown("---")
+st.markdown("<h3 style='text-align: center; color: #555555;'>👨‍💻 Developed by Swastika Barui</h3>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: #888888;'>Data Science & Analytics Portfolio</p>", unsafe_allow_html=True)
